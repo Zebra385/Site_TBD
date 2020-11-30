1ae7dd3f26bf2e1c892ad0a0ecdea7887eace7b9
houche <houcheserge@gmail.com>
2020-11-30 15:25:30 +0100

    add pages cours and stages

--- workshop/urls.py
+++ workshop/urls.py
@@ -1,10 +1,12 @@
-from .views import AccueilView
+from .views import AccueilView, CoursView, StagesView
 from django.urls import path, include
 
 app_name = "workshop"
 
 urlpatterns = [
     path('accueil/', AccueilView.as_view(), name="accueil"),
+    path('cours/', CoursView.as_view(), name="cours"),
+    path('stages/', StagesView.as_view(), name="stages"),
    
     
 ]
--- workshop/views.py
+++ workshop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -7,3 +8,17 @@ class AccueilView(TemplateView):
     """ That class to get the acceuil page"""
     template_name = "workshop/accueil.html"
     
+class CoursView(TemplateView):
+    """ That class to get the coursl page"""
+    template_name = "workshop/cours.html"
+
+    # def generate_PDF(request):
+    #     file = open('reglement.pdf', "w+b")
+    #     file.seek(0) 
+    #     pdf = file.read()
+    #     file.close() 
+    #     return HttpResponse(pdf, 'worshop/pdf')
+
+class StagesView(TemplateView):
+    """ That class to get the stages page"""
+    template_name = "workshop/stages.html"
