db1da4d6e4f99294056b5302e1b79defbed1fdc6
Zebra385 <houcheserge@gmail.com>
2021-01-12 18:04:27 +0100

    Revert "Add functionnel test"
    This reverts commit 536ecdcd48e27ce7ded4795441da1713bdb31ea4.

--- members/tests.py
+++ members/tests.py
@@ -7,69 +7,69 @@ from accounts.models import CustomUser
 from members.views import AccueilMemberView, CalendarExchangeMeeting
 
 
-# class AccueilMemberViewTestCase(TestCase):
-#     def setUp(self):
-#         """
-#         We create data in the  tables CalendarMeeting CalendarCustomuser
-#         and Customuser
-#         """
-#         all_values_customuser = read_json("customuser.json")
-#         for val in all_values_customuser:
-#             # We fill the data base members.calendarmetting
-#             CustomUser.objects.create(
-#                 password=val["fields"]["password"],
-#                 username=val["fields"]["username"],
-#                 email=val["fields"]["email"],
-#                 )
-#         # we create a user connect
-#         self.user = CustomUser.objects.get(email='houche.serge@orange.fr')
-#         all_values = read_json("calendar.json")
-#         for date in all_values:
-#             # We fill the data base members.calendarmetting
-#             CalendarMeeting.objects.create(
-#                 date=date,
-#                 )
-#         self.day_list = list(CalendarMeeting.objects.all().order_by('date'))
-#         self.calendar = calendar(self.day_list)
-#         all_values_calendar_user = read_json("calendarcustomuser.json")
-#         for date in all_values_calendar_user:
-#             auth_user = CustomUser.objects.get(pk=date["fields"]["auth_user"])
-#             date_meeting = CalendarMeeting.objects.get(
-#                 pk=date["fields"]["date_meeting"])
-#             # We fill the data base members.calendarmetting
-#             CalendarCustomuser.objects.create(
-#                 auth_user=auth_user,
-#                 date_meeting=date_meeting,
-#                 )
+class AccueilMemberViewTestCase(TestCase):
+    def setUp(self):
+        """
+        We create data in the  tables CalendarMeeting CalendarCustomuser
+        and Customuser
+        """
+        all_values_customuser = read_json("customuser.json")
+        for val in all_values_customuser:
+            # We fill the data base members.calendarmetting
+            CustomUser.objects.create(
+                password=val["fields"]["password"],
+                username=val["fields"]["username"],
+                email=val["fields"]["email"],
+                )
+        # we create a user connect
+        self.user = CustomUser.objects.get(email='houche.serge@orange.fr')
+        all_values = read_json("calendar.json")
+        for date in all_values:
+            # We fill the data base members.calendarmetting
+            CalendarMeeting.objects.create(
+                date=date,
+                )
+        self.day_list = list(CalendarMeeting.objects.all().order_by('date'))
+        self.calendar = calendar(self.day_list)
+        all_values_calendar_user = read_json("calendarcustomuser.json")
+        for date in all_values_calendar_user:
+            auth_user = CustomUser.objects.get(pk=date["fields"]["auth_user"])
+            date_meeting = CalendarMeeting.objects.get(
+                pk=date["fields"]["date_meeting"])
+            # We fill the data base members.calendarmetting
+            CalendarCustomuser.objects.create(
+                auth_user=auth_user,
+                date_meeting=date_meeting,
+                )
 
-#         self.day_list_customuser = list(
-#             CalendarCustomuser.objects.filter(
-#                 auth_user=self.user
-#                 ).order_by('date_meeting')
-#                 )
-#         self.calendar_user = calendar1(self.day_list_customuser)
-#         self.calendar_customuser = calendar_customuser(
-#             self.calendar_user,
-#             self.calendar
-#             )
+        self.day_list_customuser = list(
+            CalendarCustomuser.objects.filter(
+                auth_user=self.user
+                ).order_by('date_meeting')
+                )
+        self.calendar_user = calendar1(self.day_list_customuser)
+        self.calendar_customuser = calendar_customuser(
+            self.calendar_user,
+            self.calendar
+            )
 
-#     def test_environment_set_in_context(self):
-#         """
-#         Test the context data
-#         """
-#         request = self.client.get(
-#             '/',
-#             data={'calendar': self.calendar,
-#                   'calendar_customuser': self.calendar_customuser, })
-#         request.user = self.user
-#         view = AccueilMemberView()
-#         view.setup(request)
-#         # we fix the object _list because we do not call
-#         # SearchProduct as a view
-#         view.object_list = view.get_queryset()
-#         context = view.get_context_data()
-#         self.assertIn('calendar', context)
-#         self.assertIn('calendar_customuser', context)
+    def test_environment_set_in_context(self):
+        """
+        Test the context data
+        """
+        request = self.client.get(
+            '/',
+            data={'calendar': self.calendar,
+                  'calendar_customuser': self.calendar_customuser, })
+        request.user = self.user
+        view = AccueilMemberView()
+        view.setup(request)
+        # we fix the object _list because we do not call
+        # SearchProduct as a view
+        view.object_list = view.get_queryset()
+        context = view.get_context_data()
+        self.assertIn('calendar', context)
+        self.assertIn('calendar_customuser', context)
 
 
 class CalendarExchangeMeetingTestCase(TestCase):
